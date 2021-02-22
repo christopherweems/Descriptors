bb7904cbc0b82b8a1061cccc225edbffa7b0e5e2
Christopher Weems <hello@christopherweems>
2021-02-22 18:31:09 -0500

    `PressDescriptor` now conforms to `BooleanDescriptor`

--- Sources/Descriptors/PressDescriptor.swift
+++ Sources/Descriptors/PressDescriptor.swift
@@ -1,12 +1,16 @@
 //  PressDescriptor.swift
 //  Created by Christopher Weems on 2/22/21
 
-public enum PressDescriptor {
+public enum PressDescriptor: BooleanDescriptor {
     case pressed
     case unpressed
     
     public init(_ pressed: Bool) {
         self = pressed ? .pressed : .unpressed
+    }
+    
+    public init(booleanLiteral value: Bool) {
+        self.init(value)
     }
     
 }
